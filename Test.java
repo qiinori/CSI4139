76edc0ed2dc600181c3509fc4f1f6e2b2355f666
(Inori) Jiajie <qiinori@gmail.com>
2020-09-27 22:26:10 -0400

    update test

--- Test.java
+++ Test.java
@@ -1,3 +1,5 @@
+import java.security.KeyPair;
+
 import javax.crypto.SecretKey;
 
 public class Test {
@@ -16,6 +18,7 @@ public class Test {
         String decrypedText = SymmetricEncryption.performAESDecryption(cipherText, Symmetrickey, initial);
 
         System.out.println("The Symmetric Key is :" + new String(Symmetrickey.getEncoded()));
+        // need to fix this
         System.out.println("The Ciphertext is :" + new String(Symmetrickey.getEncoded()));
         System.out.println("The plain text is " + text);
         System.out.println("The decrypted text is :" + decrypedText);
@@ -26,6 +29,23 @@ public class Test {
          */
         System.out.println("**********************");
         System.out.println("Testing Asymmetric Encryption...");
+
+        //need to get input file
+        KeyPair kp = AsymmetricEncryption.generateRSAKeyPair();
+        //need to hash
+        //need to create signature
+        //need to verify
+        byte[] cipherText2 = AsymmetricEncryption.performRSAEncryption(text, kp.getPrivate());
+        String decrypedText2 = AsymmetricEncryption.performRSADecryption(cipherText2, kp.getPublic());
+
+        System.out.println("The public Key is :" + new String(kp.getPublic().getEncoded()));
+        System.out.println("The private Key is :" + new String(kp.getPrivate().getEncoded()));
+        //need to fix this
+        System.out.println("The Ciphertext is :" + new String(Symmetrickey.getEncoded()));
+        System.out.println("The plain text is " + text);
+        System.out.println("The decrypted text is :" + decrypedText2);
+        System.out.println("**********************");
+
 
     }
 
