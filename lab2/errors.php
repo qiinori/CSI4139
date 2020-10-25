0807c04ce0de27605013899f43ee2175cd0e53a8
(Inori) Jiajie <qiinori@gmail.com>
2020-10-25 17:43:56 -0400

    add configuration

--- lab2/errors.php
+++ lab2/errors.php
@@ -2,6 +2,10 @@
   <div class="error">
   	<?php foreach ($errors as $error) : ?>
   	  <p><?php echo $error ?></p>
+      ; Disable displaying errors to screen
+display_errors = off
+; Enable writing errors to server logs
+log_errors = on
   	<?php endforeach ?>
   </div>
 <?php  endif ?>
